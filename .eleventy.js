c65dad2371e945621676f3fa3b09e0cf5416e0b7
Sinh Nguyen <sinh@sinhn.com>
2024-07-27 06:45:16 +0700

    Update elventy cofig

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy('vault/blog');
   return {
+    pathPrefix: '/sinh-x-blog/',
     dir: {
       input: 'vault',
       output: '_site',
