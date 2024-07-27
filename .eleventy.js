1576638c987f0b98ffc16c4c53bcd3562976c6d1
Sinh Nguyen <sinh@sinhn.com>
2024-07-27 07:28:42 +0700

    Update gh actions

--- .eleventy.js
+++ .eleventy.js
@@ -3,7 +3,7 @@ module.exports = function (eleventyConfig) {
   return {
     pathPrefix: '/sinh-x-blog/',
     dir: {
-      input: 'vault',
+      input: 'vault/Areas/Stories/',
       output: '_site',
     },
   };
